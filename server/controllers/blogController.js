9af443da6f3029d0b0ac4e6e063d3c92149f93f9
Krishna Gupta <krishnaguptaa44@gmail.com>
2025-06-18 21:22:25 +0530

    Added backend server code and updated .gitignore

---
+++ server/controllers/blogController.js
@@ -0,0 +1,49 @@
+import fs from "fs";
+import imagekit from "../configs/imageKit.js";
+import Blog from "../models/Blog.js";
+
+export const addBlog = async (req, res) => {
+  try {
+    const { title, subTitle, description, category, isPublished } = JSON.parse(
+      req.body.blog
+    );
+    const imageFile = req.file;
+
+    if (!title || !description || !category || !imageFile) {
+      return res.json({ success: false, message: "Missing required fields" });
+    }
+    const fileBuffer = fs.readFileSync(imageFile.path);
+
+    //Upload image to imageKit
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/blogs",
+    });
+
+    //Optimization through imagekit URL transformation
+    const omptimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { quality: "auto" },
+        { format: "webp" },
+        { width: "1280" },
+      ],
+    });
+
+    const image = optimizedImageUrl;
+
+    await Blog.create({
+      title,
+      subTitle,
+      description,
+      category,
+      image,
+      isPublished,
+    });
+
+    res.json({ success: true, message: "Blog added successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
