a4a9f0ae84e970d8b50f49b824fbd691539cf415
Krishna Gupta <krishnaguptaa44@gmail.com>
2025-07-05 21:36:55 +0530

    Updated

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { assets, blogCategories } from '../../assets/assets';
 import Quill from 'quill';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const AddBlog = () => {
+
+  const{axios} =useAppContext();
+  const [isAdding , setIsAdding]=useState(false)
 
   const editorRef=useRef(null);
   const quillRef=useRef(null);
@@ -18,7 +23,36 @@ const AddBlog = () => {
   }
 
   const onSubmitHandler =async(e)=>{
+   try {
     e.preventDefault();
+    setIsAdding(true)
+
+    const blog={
+      title,subTitle,
+      description:quillRef.current.root.innerHTML,
+      category,isPublished
+    }
+
+    const formData=new FormData();
+    formData.append('blog',JSON.stringify(blog))
+    formData.append('image',image)
+
+    const {data}= await axios.post('/api/blog/add',formData);
+
+    if(data.success){
+      toast.success(data.message);
+      setImage(false)
+      setTitle('')
+      quillRef.current.root.innerHTML=''
+      setCategory('Startup')
+    }else{
+      toast.error(data.message)
+    }
+   } catch (error) {
+    toast.error(error.message)
+    }finally{
+      setIsAdding(false)
+    }
   }
 
   useEffect(()=>{
@@ -64,7 +98,7 @@ const AddBlog = () => {
           placeholder="Type here"
           required
           className="w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded"
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) => setSubTitle(e.target.value)}
           value={subTitle}
         />
 
@@ -94,7 +128,9 @@ const AddBlog = () => {
       <input type='checkbox' checked={isPublished} className='scale-125 cursor-pointer' onChange={e=>setIsPublished(e.target.checked)}/>
     </div>
 
-    <button type="submit" className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'>Add Blog</button>
+    <button disabled={isAdding} type="submit" className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'>
+      {isAdding ? 'Adding...' : 'Add Blog' }
+      </button>
 
 
       </div>
