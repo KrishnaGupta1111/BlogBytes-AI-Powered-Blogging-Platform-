c783f52ae1abb77bac9420c919eba7c1b1d00e68
Krishna Gupta <krishnaguptaa44@gmail.com>
2025-07-19 17:58:30 +0530

    Fixed vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "./", // important for relative paths in Vercel
+  base: "/", // important for relative paths in Vercel
   plugins: [react(), tailwindcss()],
 });
