2130f853b9812151c6b69b116a3a5a5457ea0a1f
Krishna Gupta <krishnaguptaa44@gmail.com>
2025-07-20 23:49:27 +0530

    Fixed Bugs

--- server/server.js
+++ server/server.js
@@ -7,27 +7,25 @@ import blogRouter from "./routes/blogRoutes.js";
 
 const app = express();
 
-// ✅ Connect to MongoDB
+
 await connectDB();
 
-// ✅ Use CORS – allow your Vercel frontend only
+
 app.use(
   cors({
     origin: "https://blogbytesai.vercel.app",
   })
 );
 
-// ✅ Middleware to parse JSON
+
 app.use(express.json());
 
-// ✅ Test Route
+
 app.get("/", (req, res) => res.send("API IS WORKING"));
 
-// ✅ App Routes
 app.use("/api/admin", adminRouter);
 app.use("/api/blog", blogRouter);
 
-// ✅ Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
